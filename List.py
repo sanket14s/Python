916556909579a9af1c2faeb7c4977f1ef0413056
Sanket Satpute <98686824+sanket14s@users.noreply.github.com>
2022-03-09 00:40:50 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,10 @@
+a = ["Yes", "No", "Hello", "Hii", "Bye"]
+print(a)
+print(len(a))
+print(type(a))
+print(a[2])
+print(a[2:4])
+if "Y" in a:
+    print("Yes, available")
+else:
+    print("Unavailable")
