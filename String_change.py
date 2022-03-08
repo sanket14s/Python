d1cbe42b75393735b47696baea2ba5caf79330d5
Sanket Satpute <98686824+sanket14s@users.noreply.github.com>
2022-03-09 00:17:46 +0530

    Create String_change.py

---
+++ String_change.py
@@ -0,0 +1,9 @@
+a = "Hello, Sanket..."
+b = " Hello, Sanket..."
+print(a.upper())
+print(a.lower())
+print(b.strip())
+print(a.replace("H", "O"))
+print(a.split("!"))
+print("Original="+a)
+print("original="+b)
