daa37caeb2b72105ed078a57000e1128b1af41ef
Sanket Satpute <98686824+sanket14s@users.noreply.github.com>
2022-03-08 21:38:30 +0530

    Create Type_Conversion.py

---
+++ Type_Conversion.py
@@ -0,0 +1,26 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+#int to complex
+d = complex(z)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(d)
+print(type(d))
