b65831a1f882d631d9f1ac3d962407d73d4f5a60
Sanket Satpute <98686824+sanket14s@users.noreply.github.com>
2022-03-12 16:56:46 +0530

    Update List.py

--- List.py
+++ List.py
@@ -1,4 +1,5 @@
 a = ["Yes", "No", "Hello", "Hii", "Bye"]
+b = ["Apple", "Banana", "Pineapple"]
 print(a)
 print(len(a))
 print(type(a))
@@ -13,4 +14,7 @@ print(a)
 a[1:3] = ["Sanjivani", "Kopargaon"]
 print(a)
 a.insert(2, "Good_Night")
+print(a)
+
+a.extend(b)
 print(a)
